5cef1851921f2c0861f52f14ac7587d217dbd1d8
Vindhya Hegde <Vindhyasadanand@gmail.com>
2021-05-23 07:26:18 +0530

    Deploy my react app to github pages

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "👩‍🎓",
+    name: "Woman Student",
+    meaning:
+      "The Woman Student emoji is a ZWJ sequence combining 👩 Woman, ‍ Zero Width Joiner and 🎓 Graduation Cap. These display as a single emoji on supported platforms."
+  },
+  {
+    id: 3,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 4,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 5,
+    emoji: "👼",
+    name: "Baby Angel",
+    meaning:
+      "An angelic baby with wings, and a halo above its head. Commonly known as a cherub."
+  },
+  
+  {
+    id: 6,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "A friendly,looking straight ahead. Depicted as the face of a dog of various breeds, generally light-brown and white, with pointed or floppy ears and its tongue hanging out."
+  }
+
+];
+export default emojipedia
