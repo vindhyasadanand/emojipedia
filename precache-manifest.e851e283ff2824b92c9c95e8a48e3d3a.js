876b9c476726d520f9c3ff4e349ad1d9db1f5758
Vindhya Hegde <Vindhyasadanand@gmail.com>
2021-05-23 07:49:22 +0530

    Updates

---
+++ precache-manifest.e851e283ff2824b92c9c95e8a48e3d3a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1331b108d6cea89e187ad7169d0d76",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/emojipedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "8024c4cadedc7a03f80f",
+    "url": "/emojipedia/static/js/main.aee52028.chunk.js"
+  },
+  {
+    "revision": "7ea935b83df6c53ba113",
+    "url": "/emojipedia/static/js/runtime-main.d52a7a0e.js"
+  }
+]);
